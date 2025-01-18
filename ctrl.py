0e327b63a8a6b5a55fc499a5e4990ae5fb570582
mirinai <tmddjs1771@gmail.com>
2025-01-18 13:39:14 +0900

    Modify ctrl.py to connect sum operation to display

--- ctrl.py
+++ ctrl.py
@@ -1,4 +1,4 @@
-# ch 5.2.1 ctrl.py
+# ch 6.6.2 ctrl.py
 
 class Control:
     def __init__(self, view):
@@ -6,15 +6,21 @@ class Control:
         self.connectSignals()
     
     def calculate(self): # calculate 메서드 추가. 내용은 나중에 만들기
-        pass
+        num1 = float(self.view.le1.text()) # 첫번째 라인 에디트에 입력된 숫자를 읽어옴
+        num2 = float(self.view.le2.text()) # 두번째 라인 에디트에 입력된 숫자를 읽어옴
+        operator = self.view.cb.currentText() # 콤보박스에 선택된 연산자 확인
+
+        if operator == '+': #연산자가 '+'면 결과를 스트링으로 리턴
+            return f'{num1} + {num2} = {self.sum(num1, num2)}'
+        else:
+            return "Calculation Error"
+        
     
-    def connectSignals(self):
-        self.view.btn1.clicked.connect(self.calculate)
+    def connectSignals(self): #btn1을 클릭하면 calculate결과가 화면에 표시되도록 수정
+        self.view.btn1.clicked.connect(lambda:self.view.setDisplay(self.calculate()))
         self.view.btn2.clicked.connect(self.view.clearMessage)
     
     def sum(self, a, b): # 덧셈 함수 추가
-        try:
-            return str(a+b)
-        except:
-            return "Calculation Error"
+        return str(a+b)
+        
     
